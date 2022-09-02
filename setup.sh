f7a977dd701beea87a6dde2e96f9ab217c92764d
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-09-02 20:12:15 -0300

    theme

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]
+# Primary color
+primaryColor = '#924078'
+backgroundColor='#000908'
+secondaryBackgroundColor='#28426c'
+textColor='#fffff'
+font='sans serif'
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
