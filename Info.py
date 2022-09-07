12a136aa61579a45dc812c2403e40467316f2c2f
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-09-06 21:01:46 -0300

    info

--- Info.py
+++ Info.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 def info():
     st.markdown('---')
+    st.title('NO info is kept or stored!')
+    st.text('All info is read in cache, send to make all maths and give it back, it\'s deleted once you refresh or leave the page!')
+    st.text('Click "LolCNFT" below to head to the website and contact me!')
     st.header('General Graphs')
     st.subheader('Volume by Categories')
     st.write('This pie chart show how much % you moved over the selected period of time. This categories are taken '
